f11d48b1998339ad03122a888f9df463cadb0f87
David <nanjiladavid2@gmail.com>
2025-04-24 14:47:25 +0300

    add . to models to fix no mudule called models
    Signed-off-by: David <nanjiladavid2@gmail.com>

--- clients/admin.py
+++ clients/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Client, HealthProgram
+from .models import Client, HealthProgram
 # Register your models here.
 @admin.register(HealthProgram)
 class HealthProgramAdmin(admin.ModelAdmin):
