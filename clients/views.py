2549b45916d040a46de288c8a41bc02d855ef3a0
David <nanjiladavid2@gmail.com>
2025-04-24 14:27:06 +0300

    Add client search and detail view
    Signed-off-by: David <nanjiladavid2@gmail.com>

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import DetailView
+from .models import Client
+from django.db.models import Q
+
+def client_search(request):
+    """View to search for clients by name."""
+    query = request.GET.get('q', '')
+    clients = Client.objects.all()
+    if query:
+        clients = clients.filter(
+            Q(first_name__icontains=query) | Q(last_name__icontains=query)
+        )
+    return render(request, 'clients/client_search.html', {
+        'clients': clients,
+        'query': query
+    })
+
+class ClientDetailView(DetailView):
+    """View to display a client's profile."""
+    model = Client
+    template_name = 'clients/client_detail.html'
+    context_object_name = 'client'
