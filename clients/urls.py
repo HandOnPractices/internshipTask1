5babda7f8cf648ff89466e8c12ea73b910c6a7b2
David <nanjiladavid2@gmail.com>
2025-04-26 15:19:41 +0300

    Update API URLs for HealthProgram and Client CRUD endpoints

--- clients/urls.py
+++ clients/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('search/', views.client_search, name='client_search'),
     path('client/<int:pk>/', views.ClientDetailView.as_view(), name='client_detail'),
-    path('api/client/<int:pk>/', views.ClientDetailAPIView.as_view(), name='client_detail_api'),
-
+    # API URLs
+    path('api/programs/', views.HealthProgramListCreateAPIView.as_view(), name='program_list_create_api'),
+    path('api/programs/<int:pk>/', views.HealthProgramRetrieveUpdateDestroyAPIView.as_view(), name='program_detail_api'),
+    path('api/clients/', views.ClientListCreateAPIView.as_view(), name='client_list_create_api'),
+    path('api/clients/<int:pk>/', views.ClientRetrieveUpdateDestroyAPIView.as_view(), name='client_detail_api'),
 ]
