464169f7c41d1fbf905fc645987dc48701e50a91
David <nanjiladavid2@gmail.com>
2025-04-26 19:04:35 +0300

    Add client_create URL pattern

--- clients/urls.py
+++ clients/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('search/', views.client_search, name='client_search'),
     path('program/create/', views.program_create, name='program_create'),
+    path('client/create/', views.client_create, name='client_create'),
     path('client/<int:pk>/', views.ClientDetailView.as_view(), name='client_detail'),
     # API URLs
     path('api/programs/', views.HealthProgramListCreateAPIView.as_view(), name='program_list_create_api'),
